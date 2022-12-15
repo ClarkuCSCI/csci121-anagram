7ad0208ff88e9995a191e4228f298cc3428875c1
quinnfedyk <120613396+quinnfedyk@users.noreply.github.com>
2022-12-15 00:19:08 -0500

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -14,6 +14,6 @@ public class Anagram {
     }
 
     public static void main(String[] args) {
-        System.out.println(anagram(args[0], args[1]));
+        System.out.println(anagram(args[0].toLowerCase(), args[1].toLowerCase()));
     }
 }
