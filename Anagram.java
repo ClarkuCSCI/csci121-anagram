7c31520f6978b37bca9b06924f6116bfbbb1533d
Billie-McCarthy <119871473+Billie-McCarthy@users.noreply.github.com>
2023-04-27 18:31:33 -0400

    fixed bug
    hash tables now take into account how frequently a character occurs.

--- Anagram.java
+++ Anagram.java
@@ -2,13 +2,27 @@ import java.util.HashMap;
 
 public class Anagram {
     public static boolean anagram(String oneS, String twoS) {
-        HashMap<Character, Boolean> oneHM = new HashMap<Character, Boolean>();
-        HashMap<Character, Boolean> twoHM = new HashMap<Character, Boolean>();
+        HashMap<Character, Integer> oneHM = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> twoHM = new HashMap<Character, Integer>();
         for (char c : oneS.toCharArray()) {
-            oneHM.put(c, true);
+            int occurrence;
+            if (oneHM.get(c) != null) {
+                occurrence = oneHM.get(c);
+            }
+            else {
+                occurrence = 1;
+            }
+            oneHM.put(c, occurrence + 1);
         }
         for (char c : twoS.toCharArray()) {
-            twoHM.put(c, true);
+            int occurrence;
+            if (twoHM.get(c) != null) {
+                occurrence = twoHM.get(c);
+            }
+            else {
+                occurrence = 1;
+            }
+            twoHM.put(c, occurrence + 1);
         }
         return oneHM.equals(twoHM);
     }
